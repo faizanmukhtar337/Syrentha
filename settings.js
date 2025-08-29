74bdf3a8741833693b66c350e189ffcac7d1fb6b
unknown <syrenthaofficial@gmail.com>
2025-08-29 13:48:06 +0100

    Initial commit: Syrentha WhatsApp bot

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'ꜱʏʀᴇɴᴛʜᴀ',
+  author: '‎',
+  botName: "ꜱʏʀᴇɴᴛʜᴀ",
+  botOwner: '𝓕𝓪𝓲𝔃𝓪𝓷', // Your name
+  ownerNumber: '923226070203', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.1.1",
+};
+
+module.exports = settings;
